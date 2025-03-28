5ba17371de8a66de58ac82fd9b832b54c30c3d8d
s-akdemir <selman.akdemir@outlook.com>
2025-03-28 15:39:45 +0300

    test spec completed
    test spec completed

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,20 +3,20 @@ describe("Login test pages", () => {
           cy.visit('http://localhost:5173/');
       });  
   it('should show error message for wrong email entry', () => {
-      cy.get('input[type="email"]').type("selman");
-      cy.get('input[type="password"]').type("1234567Abc");
+    cy.get('input[name="email"]').type("selman");
+    cy.get('input[name="password"]').type("1234567Abc");
       cy.get('button[color="primary"]').should('be.disabled');
       cy.contains('Please enter a valid email address').should('be.visible');
   });
   it('should show error message for short password and wrong email', () => {
-    cy.get('input[type="email"]').type("selman");
-    cy.get('input[type="password"]').type("123");
+    cy.get('input[name="email"]').type("selman");
+    cy.get('input[name="password"]').type("123");
     cy.contains('Please enter a valid email address').should('be.visible');
     cy.contains('Password must be at least 4 characters long').should('be.visible');
   });
-  it('should show error message for long password', () => {
-      cy.get('[data-cy="input-password"]').type("12348749837984739874938749837498739487398473984");
-      cy.contains('Şifreniz 12 karakterden uzun olmamalı!').should('be.visible');
+  it('should disable button if terms not checked', () => {
+      cy.get('input[name="terms"]').should('not.be.checked');
+      cy.get('button[color="primary"]').should('be.disabled');
   });
   });
   
