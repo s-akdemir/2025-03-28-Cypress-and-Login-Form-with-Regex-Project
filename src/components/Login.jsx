e8bcb765529820cc9a3e33b0d42b7ed40c468593
s-akdemir <selman.akdemir@outlook.com>
2025-03-28 14:42:25 +0300

    Initial commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  FormFeedback,
+} from 'reactstrap';
+import { useHistory } from 'react-router-dom';
+
+import axios from 'axios';
+
+const initialForm = {
+  email: '',
+  password: '',
+  terms: false,
+};
+
+const errorMessages = {
+  email: 'Please enter a valid email address',
+  password: 'Password must be at least 4 characters long',
+  terms: 'you must accept the terms',
+};
+
+export default function Login() {
+  const [form, setForm] = useState(initialForm);
+  const [isValid, setIsValid] = useState(false);
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const isValidFunction = () => {
+    const newErrors = {};
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+    if (!emailRegex.test(form.email)) {
+      newErrors.email = errorMessages.email;
+    }
+    if (form.password.length < 4) {
+      newErrors.password = errorMessages.password;
+    }
+    if (!form.terms) {
+      newErrors.terms = errorMessages.terms;
+    }
+    setErrors(newErrors);
+    if (Object.keys(newErrors).length === 0) {
+      setIsValid(true);
+    }
+  };
+  useEffect(() => {
+    isValidFunction();
+  }, [form]);
+
+  const handleChange = (event) => {
+    let { name, value, type } = event.target;
+    value = type === 'checkbox' ? event.target.checked : value;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isValid) {
+      axios
+        .get('https://6540a96145bedb25bfc247b4.mockapi.io/api/login')
+        .then((res) => {
+          const user = res.data.find(
+            (item) => item.password == form.password && item.email == form.email
+          );
+          if (user) {
+            setForm(initialForm);
+            history.push('/main');
+          } else {
+            history.push('/error');
+          }
+        });
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for="exampleEmail">Email</Label>
+        <Input
+          id="exampleEmail"
+          name="email"
+          placeholder="Enter your email"
+          type="email"
+          onChange={handleChange}
+          value={form.email}
+          invalid={errors.email}
+        />
+        {errors.email && <FormFeedback>{errors.email}</FormFeedback>}
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input
+          id="examplePassword"
+          name="password"
+          placeholder="Enter your password "
+          type="password"
+          onChange={handleChange}
+          value={form.password}
+          invalid={errors.password}
+        />
+        {errors.password && <FormFeedback>{errors.password}</FormFeedback>}
+      </FormGroup>
+      <FormGroup check>
+        <Input
+          id="terms"
+          name="terms"
+          checked={form.terms}
+          type="checkbox"
+          onChange={handleChange}
+        />{' '}
+        <Label htmlFor="terms" check>
+          I agree to terms of service and privacy policy
+        </Label>
+      </FormGroup>
+      <FormGroup className="text-center p-4">
+        <Button disabled={!isValid} color="primary">
+          Sign In
+        </Button>
+      </FormGroup>
+    </Form>
+  );
+}
